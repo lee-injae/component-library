d49dd05b82f4ab3ca10875b18dec8471c82f64da
Injae Lee <ilee.injae@gmail.com>
2023-11-03 16:03:57 -0400

    banner render function and refactoring completed

--- src/App.jsx
+++ src/App.jsx
@@ -9,31 +9,35 @@ const badgeColors = ["gray", "red", "yellow", "green", "blue", "indigo", "purple
 
 const bannerData = [
   {
-    "title" : "success",
+    "variant" : "success",
     "color" : "green", 
     "imgUrl" : "/assets/check-circle.png",
+    "alt" : "icon",
     "msg": "Congratulations!",
     "subtitle": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."
   },
   {
-    "title" : "warning",
+    "variant" : "warning",
     "color" : "yellow",
-    "imgUrl" : "/assets/check-circle.png",
+    "imgUrl" : "/assets/warning-exclamation.png",
+    "alt" : "icon",
     "msg": "Attention",
     "subtitle": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum."
   }, 
   {
-    "title" : "error",
+    "variant" : "error",
     "color" : "red",
-    "imgUrl" : "/assets/check-circle.png",
+    "imgUrl" : "/assets/x-circle.png",
+    "alt" : "icon",
     "msg": "There is a prioblem with your application",
     "subtitle": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum."
 
   }, 
   {
-    "title" : "neutral",
+    "variant" : "neutral",
     "color" : "indigo",
     "imgUrl" : "/assets/blue-info.png",
+    "alt" : "icon",
     "msg": "Update available",
     "subtitle": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."
   }
@@ -54,15 +58,35 @@ function App() {
     ))
   }
 
-  const hehe = bannerData.map( data => (
-    <Banner key={`banner-${data.color}`} color={data.color}>
-      <img src={data.imgUrl}/>
-      {data.msg}
-    </Banner>
-  )
+  const createBanners = (bannerData, includeSubtitle = false) => {
+    return bannerData.map( data => (
+      <Banner key={`banner-${data.variant}`} className="banner" color={data.color}>
+        <img src={data.imgUrl} alt={data.alt}/>
+        <div>
+          {data.msg}
+          {includeSubtitle && data.subtitle && <p>{data.subtitle}</p>}
+        </div>
+      </Banner>
+    ))
+  }
 
-  )
+  const bannersWithoutSubtitles = createBanners(bannerData)
+  const bannersWithSubtitles = createBanners(bannerData, true)
 
+  // const createSingleLineBanner = bannerData.map( data => (
+  //   <Banner key={`banner-${data.color}`} className="banner" color={data.color}>
+  //     <img src={data.imgUrl}/>
+  //     {data.msg}
+  //   </Banner>
+  // ))
+
+  // const createMultiline = bannerData.map( data => (
+  //   <Banner key={`banner-${data.color}`} className="banner" color={data.color}>
+  //     <img src={data.imgUrl}/>
+  //     {data.msg}
+  //     <p>{data.subtitle}</p>
+  //   </Banner>
+  // ))
 
 
   const squareBadges = createBadges("square")
@@ -83,11 +107,8 @@ function App() {
       </div>
       <div>
         <h1>BANNERS</h1>
-        {/* <Banner className="banner" color="green">
-          <img src="/assets/check-circle.png" alt="green-check-circle"/>
-          Congratulations!
-        </Banner> */}
-        {hehe}
+        {bannersWithoutSubtitles}
+        {bannersWithSubtitles}
       </div>
     </>
   )
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import classnames from "classnames"
 
-export default function Banner({ children, className, color }){
+export default function Banner({ children, color, className }){
     const colorClass = (color) && `banner-${color}`
     const allClasses = classnames(colorClass, className)
 
