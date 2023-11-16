7b745d788b2356e996f2b52c234f1b20616d3aca
Injae Lee <ilee.injae@gmail.com>
2023-11-16 13:42:48 -0500

    Initiated creating card component

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,28 @@ import React from "react"
 import './App.css'
 import Badge from "./components/Badge/Badge"
 import Banner from "./components/Banner/Banner"
-
+import Card from "./components/Card/Card"
 
 
 const badgeColors = ["gray", "red", "yellow", "green", "blue", "indigo", "purple", "pink" ]
 
 const bannerData = [
   {
-    "variant" : "success",
+    "status" : "success",
     "imgUrl" : "/assets/check-circle.png",
     "alt" : "icon",
     "msg": "Congratulations!",
     "subtitle": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."
   },
   {
-    "variant" : "warning",
+    "status" : "warning",
     "imgUrl" : "/assets/warning-exclamation.png",
     "alt" : "icon",
     "msg": "Attention",
     "subtitle": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum."
   }, 
   {
-    "variant" : "error",
+    "status" : "error",
     "imgUrl" : "/assets/x-circle.png",
     "alt" : "icon",
     "msg": "There is a prioblem with your application",
@@ -31,7 +31,7 @@ const bannerData = [
 
   }, 
   {
-    "variant" : "neutral",
+    "status" : "neutral",
     "imgUrl" : "/assets/blue-info.png",
     "alt" : "icon",
     "msg": "Update available",
@@ -56,7 +56,9 @@ function App() {
 
   const createBanners = (bannerData, includeSubtitle = false) => {
     return bannerData.map( data => (
-      <Banner key={`banner-${data.variant}`} className="banner" variant={data.variant}>
+      <Banner key={`banner-${data.status}`} 
+              className="banner" 
+              status={data.status}>
         <img src={data.imgUrl} alt={data.alt}/>
         <div>
           {data.msg}
@@ -68,23 +70,6 @@ function App() {
 
   const bannersWithoutSubtitles = createBanners(bannerData)
   const bannersWithSubtitles = createBanners(bannerData, true)
-
-  // const createSingleLineBanner = bannerData.map( data => (
-  //   <Banner key={`banner-${data.color}`} className="banner" color={data.color}>
-  //     <img src={data.imgUrl}/>
-  //     {data.msg}
-  //   </Banner>
-  // ))
-
-  // const createMultiline = bannerData.map( data => (
-  //   <Banner key={`banner-${data.color}`} className="banner" color={data.color}>
-  //     <img src={data.imgUrl}/>
-  //     {data.msg}
-  //     <p>{data.subtitle}</p>
-  //   </Banner>
-  // ))
-
-
   const squareBadges = createBadges("square")
   const pillBadges = createBadges("pill")
 
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 import classnames from "classnames"
 
-export default function Banner({ children, variant, className }){
-    const colorClass = (variant) && `banner-${variant}`
+export default function Banner({ children, status, className }){
+    const colorClass = (status) && `banner-${status}`
     const allClasses = classnames(colorClass, className)
 
     return(
